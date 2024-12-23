53e6a5e149e6d8f4a5c774aa4a82ed1fac8f6e5b
Rohit Anand <rohit.anand@woven-planet.global>
2024-12-23 10:20:11 +0530

    create a repo to connect to replit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,12 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
-import java.util.Arrays;
-
 
 public class Main {
   public static void main(String[] args) {
-    
-    // MergeSort 
-    int[] arr = {5, 2, 4, 6, 1, 3};
-    MergeSort.mergeSort(arr, 0, arr.length - 1);
-    System.out.println(Arrays.toString(arr));
-    
     System.out.println("Hello world!");
+
+    // Merge Sort
+    MergeSort.main(args);
   }
 }
